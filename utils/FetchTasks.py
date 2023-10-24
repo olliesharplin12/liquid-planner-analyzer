473988cc213d9a8d40774ce354f4a27ac348396d
Ollie Sharplin <oliver.sharplin@infact.co.nz>
2023-10-24 21:09:32 +1300

    Partial implementations of LP API fetch methods

--- utils/FetchTasks.py
+++ utils/FetchTasks.py
@@ -1,37 +1,39 @@
-import requests
 from typing import List
 
 from ..classes.Task import Task
-from ..utils.Utils import build_url
+from ..utils.Utils import fetch
 
 
-BASE_URL = ''    # TODO
-FETCH_TASKS_URL = f'{BASE_URL}/'  # TODO
-FETCH_SPRINT_TASKS_URL = f'{BASE_URL}/'  # TODO
+FETCH_TIMESHEET_ENTRIES_URL = 'timesheet_entries'
+FETCH_SPRINT_TASKS_URL = ''  # TODO
 
 def fetch_tasks_by_filters(start: str, end: str, user_ids: List[str] = None) -> List[Task]:
-    query_params: List[tuple[str, str]] = [('start', start), ('end', end)]
-
-    if user_ids is type(List):
-        query_params.append(('users', ','.join(user_ids)))  # TODO
-
-    url = build_url(FETCH_TASKS_URL, query_params)
-    response = requests.get(url)
-
+    query_params: List[tuple[str, str]] = [('start_date', start), ('end_date', end)]
+    
+    response = fetch(FETCH_TIMESHEET_ENTRIES_URL, query_params)
     if response.status_code != 200:
-        raise Exception(f'Response Error: {response.message}')  # TODO
+        raise Exception(f'Response Error: {response.text}')
+    
+    # TODO: Evaluate response JSON and formulate into tasks.
+
+    timesheet_entires = 
 
     tasks = [Task() for task in response.json()]
+
+    # Filter tasks by user ids
+    if user_ids is type(List) and len(user_ids) > 0:
+        query_params.append(('users', ','.join(user_ids)))  # TODO
+
     return tasks
 
 def fetch_tasks_at_sprint_date(sprint: int, date: str) -> List[Task]:
     query_params: List[tuple[str, str]] = [('sprint', f'Sprint {sprint}'), ('date', date)]
 
-    url = build_url(FETCH_SPRINT_TASKS_URL, query_params)
-    response = requests.get(url)
-
+    response = fetch(FETCH_SPRINT_TASKS_URL, query_params)
     if response.status_code != 200:
-        raise Exception(f'Response Error: {response.message}')  # TODO
+        raise Exception(f'Response Error: {response.text}')
+    
+    # TODO: Evaluate response JSON and formulate into tasks.
     
     tasks = [Task() for task in response.json()]
     return tasks
--- utils/Utils.py
+++ utils/Utils.py
@@ -1,8 +1,17 @@
+import requests
+from requests import Response
 from typing import List
 
-def build_url(url: str, query_params: List[tuple[str, str]]) -> str:
+WORKSPACE_ID = ""  # TODO
+BASE_URL = 'https://app.liquidplanner.com/api/v1/workspaces/{}/'  # TODO
+
+def build_url(url_suffix: str, query_params: List[tuple[str, str]]) -> str:
     if len(query_params) > 0:
         # TODO: Convert to url encoded string.
-        return url + '?' + '&'.join([f'{key}={value}' for key, value in query_params])
+        return BASE_URL + url_suffix + '?' + '&'.join([f'{key}={value}' for key, value in query_params])
     else:
-        return url
+        return BASE_URL + url_suffix
+
+def fetch(url_suffix, query_params: List[tuple[str, str]]) -> Response:
+    url = build_url(url_suffix, query_params)
+    return requests.get(url)
